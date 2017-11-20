d27fb17b77fbbf1d2bc007db0ccb9fa6ba483996
karolskora1993 <karol-skora@poczta.pl>
2017-11-20 10:03:42 +0100

    Initial Commit

---
+++ Monte Carlo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Monte Carlo
+//
+//  Created by apple on 20.11.2017.
+//  Copyright © 2017 apple. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
